1b5adf8f0ba28529a69f1ae8eb9ede36e3edac4b
Olly <developer.olly@gmail.com>
2022-01-29 14:55:05 +0100

    feat: modified button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
-interface Props {
+interface IProps {
   buttonClass?: string;
   children?: React.ReactNode;
   onClick: () => void;
 }
-const Button: React.FC<Props> = ({
+const Button = ({
   buttonClass = 'px-4 py-2 bg-pink-600 rounded-md text-white',
   children,
   onClick,
-}) => {
-  return <button className={buttonClass}> {children} </button>;
+}: IProps): JSX.Element => {
+  return (
+    <button className={buttonClass} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
